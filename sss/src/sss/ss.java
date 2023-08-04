abcde13043e9ce09b2a2b6caf6acf9f5b774a707
mairajuddin.mohd <mairajuddin.mohd@UNIV64.induseoffice.com>
2023-08-04 12:15:56 +0530

    fgf

--- sss/src/sss/ss.java
+++ sss/src/sss/ss.java
@@ -1,8 +1,32 @@
 package sss;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ss {
 public static void main(String a[]) {
-	
-	System.out.println(" sdfsaf sdfs");
+	try {
+
+        URL url = new URL("http://192.168.1.166:8080/mydashboard/apb");//your url i.e fetch data from .
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP Error code : "
+                    + conn.getResponseCode());
+        }
+        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+        BufferedReader br = new BufferedReader(in);
+        String output;
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+        conn.disconnect();
+
+    } catch (Exception e) {
+        System.out.println("Exception in NetClientGet:- " + e);
+    }
 }
 }
